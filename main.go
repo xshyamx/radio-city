45b4072a3d9645c408cfa757102caaf51bb8e526
shyam <xshyamx@gmail.com>
2018-12-05 05:23:57 +0530

    using a config url instead of hardcoded values

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"html/template"
@@ -9,6 +10,8 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+
+	"github.com/pkg/errors"
 )
 
 func RSSScrapeHandler(url string) http.HandlerFunc {
@@ -21,7 +24,6 @@ func RSSScrapeHandler(url string) http.HandlerFunc {
 			w.Write([]byte("Failed to scrape feed data"))
 			return
 		}
-		fmt.Println(r)
 		for i, _ := range rss.Channels {
 			rss.Channels[i].AtomLink = NewAtomLink(fmt.Sprintf("%s://%s%s", r.Header.Get("X-Forwarded-Proto"), r.Host, r.URL.String()))
 		}
@@ -62,21 +64,38 @@ func IndexHandler(podcasts []Podcast) http.HandlerFunc {
 }
 
 type Podcast struct {
-	Path string
-	Name string
-	URL  string
+	Path string `json:"prefix"`
+	Name string `json:"name"`
+	URL  string `json:"url"`
+}
+
+func getPodcasts() ([]Podcast, error) {
+	configUrl := os.Getenv("CONFIG_URL")
+	if configUrl == "" {
+		configUrl = "https://api.myjson.com/bins/mn5mu"
+	}
+	var podcasts []Podcast
+	buf, err := loadUrl(configUrl)
+	if err != nil {
+		return podcasts, errors.Wrapf(err, "Failed to retrieve url %s", configUrl)
+	}
+	if err := json.Unmarshal(buf, &podcasts); err != nil {
+		return podcasts, errors.Wrapf(err, "Failed to parse %s as json", string(buf))
+	}
+	return podcasts, nil
 }
 
 func main() {
+	logger := log.New(os.Stdout, "", 0)
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt)
 	addr := ":" + os.Getenv("PORT")
 	if addr == ":" {
 		addr = ":8080"
 	}
-	podcasts := []Podcast{
-		Podcast{"/cd", "Crime Diary", "https://www.radiocity.in/radiocity/show-podcasts-tamil/Crime-Diary/153"},
-		Podcast{"/kck", "Kissa Crime Ka", "https://www.radiocity.in/radiocity/show-podcasts-hindi/Kissa-Crime-Ka/82"},
+	podcasts, err := getPodcasts()
+	if err != nil {
+		logger.Fatalf("Failed to retrieve any podcast configurations\n%q", err)
 	}
 
 	mux := http.NewServeMux()
@@ -85,7 +104,6 @@ func main() {
 		mux.HandleFunc(podcast.Path, RSSScrapeHandler(podcast.URL))
 	}
 	h := &http.Server{Addr: addr, Handler: mux}
-	logger := log.New(os.Stdout, "", 0)
 
 	go func() {
 		logger.Printf("Listening on http://0.0.0.0%s\n", addr)
@@ -100,4 +118,5 @@ func main() {
 	logger.Println("\nShutting down the server...")
 	h.Shutdown(context.Background())
 	logger.Println("Server gracefully stopped")
+
 }
